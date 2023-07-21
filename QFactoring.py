f14069c98f775c9f63402de9e406c61a659d2bd3
brandonyee-cs <139765638+brandonyee-cs@users.noreply.github.com>
2023-07-20 21:04:56 -0400

    Add files via upload

---
+++ QFactoring.py
@@ -0,0 +1,64 @@
+from math import sqrt
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def simplify_fraction(numer, denom):
+    if denom == 0:
+        return "Division by 0 - result undefined"
+
+    # Remove greatest common divisor:
+    common_divisor = gcd(numer, denom)
+    (reduced_num, reduced_den) = (numer / common_divisor, denom / common_divisor)
+    # Note that reduced_den > 0 as documented in the gcd function.
+
+    if common_divisor == 1:
+        return (numer, denom)
+    else:
+        # Bunch of nonsense to make sure denominator is negative if possible
+        if (reduced_den > denom):
+            if (reduced_den * reduced_num < 0):
+                return(-reduced_num, -reduced_den)
+            else:
+                return (reduced_num, reduced_den)
+        else:
+            return (reduced_num, reduced_den)
+
+def quadratic_factoring(a,b,c):
+    if (b**2-4*a*c >= 0):
+        x1 = (-b+sqrt(b**2-4*a*c))/(2*a)
+        x2 = (-b-sqrt(b**2-4*a*c))/(2*a)
+        # Added a "-" to these next 2 values because they would be moved to the other side of the equation
+        mult1 = -x1 * a
+        mult2 = -x2 * a
+        (num1,den1) = simplify_fraction(a,mult1)
+        (num2,den2) = simplify_fraction(a,mult2)
+        if ((num1 > a) or (num2 > a)):
+            # simplify fraction will make too large of num and denom to try to make a sqrt work
+            print("No factorization")
+        else:
+            # Getting ready to make the print look nice
+            if (den1 > 0):
+                sign1 = "+"
+            else:
+                sign1 = ""
+            if (den2 > 0):
+                sign2 = "+"
+            else:
+                sign2 = ""
+            print("({}x{}{})({}x{}{})".format(int(num1),sign1,int(den1),int(num2),sign2,int(den2)))
+    else:
+        # if the part under the sqrt is negative, you have a solution with i
+        print("Solutions are imaginary")
+    return
+
+    
+# This function takes in a, b, and c from the equation:
+# ax^2 + bx + c
+# and prints out the factorization if there is one
+Value1 = float(input("Enter Value A:"))
+Value2 = float(input("Enter Value B:"))
+Value3 = float(input("Enter Value C:"))
+quadratic_factoring(Value1,Value2,Value3)
